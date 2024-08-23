9fd890d00ba638951b6feeeffdc5ed8f451847bc
Elias Garcia Carvajal <ega.carvajal@gmail.com>
2024-08-22 23:51:35 -0400

    Configuracion demo docker compose con nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: process.env.ENV_FILE || '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  await app.listen(3000, () => {
+    logger.log('=====================================================');
+    logger.log('Application has started successfully');
+    logger.log('=====================================================');
+  });
 }
 bootstrap();
